450625dd163252ece03f7b50212a9c087dda503a
SangHui48 <vodlrm123@naver.com>
2021-02-23 22:42:55 +0900

    파이썬 기초 문법 공부

---
+++ nkrqb/test.py
@@ -0,0 +1,19 @@
+# print('hello')
+# print("apple;hello;")
+
+# q6 = "Strawberry"
+# print(q6[::-1])
+
+# a = [1,2,3]
+# b = [4,5]
+# print(a+b)
+
+# e = (10, 100, ('Pen', 'Cap', 'Plate'))
+# # print('e - ', e[-1][1])
+# # print('e - ', list(e[-1][1]))
+
+# print(type(e[0:2]))
+
+a = {'name': 'Kim', 'phone': '01012345678', 'birth': '870124'}
+
+print(list(a.keys()))
